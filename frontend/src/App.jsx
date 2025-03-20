8423ea2cc54cb9f852e909664515d3d160afc001
Jeff <jeffbohn@my.cwi.edu>
2025-03-20 11:15:12 -0600

    Install React Router, create Admin page, update README

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,40 @@
-import './App.css'
+//import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx'
 import MissionStatement from './components/MissionStatement.jsx'
 import Appointments from './components/Appointments.jsx'
 import Services from './components/Services.jsx'
+import AdminHomePage from './admin/pages/AdminHomePage.jsx';
 
 
 function App() {
   return (
-    <>
-        <Navbar />
-        <MissionStatement />
-        <Appointments />
-        <Services />
-    </>
+      <>
+      <Routes>
+        {/* Render your main content components here for the root path */}
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <MissionStatement />
+            <Appointments />
+            <Services />
+          </>
+        }/>
+        <Route path="/admin" element={<AdminHomePage />} />
+      </Routes>
+      </>
   )
 }
 
 export default App
 
 
-
+//<>
+//<Navbar />
+//<MissionStatement />
+//<Appointments />
+//<Services />
+//</>  
 
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // <Router>
---
+++ frontend/src/admin/pages/AdminHomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+// This page WILL NEED TO change browser tab title to 'ADMIN : Scotts Collision Repair'
+
+export default function AdminHomePage() {
+  return (
+     <div className='d-flex justify-content-between align-items-center bg-black p-4'>
+          <div className='text-white fs-4'>Admin Home Page</div>
+    
+          <Link to="/" className="m-0 text-end">
+            <button className='btn btn-outline-light'>Home</button>
+          </Link>
+     </div>
+  )
+}
+
