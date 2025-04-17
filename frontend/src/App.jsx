019c3174e958cd1497b31ec057e9047da9867a2c
marco_ramos <ramosmarco95@gmail.com>
2025-04-16 22:25:44 -0600

    Added 404Page. Updated routes for clean structure in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes,  Route} from 'react-router-dom';
+
+// Components
 import Navbar from './components/Navbar.jsx'
 import MissionStatement from './components/MissionStatement.jsx'
 import Appointments from './components/Appointments.jsx'
 import Services from './components/Services.jsx'
-import ContactPage from './pages/ContactPage.jsx'
 
-
+// Pages
 import AdminHomePage from './admin/pages/AdminHomePage.jsx';
 import AdminReviewsPage from './admin/pages/AdminReviewsPage.jsx';
 import AdminAppointmentsPage from './admin/pages/AdminAppointmentsPage.jsx';
@@ -14,88 +16,33 @@ import LocationPage from './pages/LocationPage.jsx';
 import CustPaintPage from './pages/CustPaintPage.jsx';
 import PaintDetailsPage from './pages/PaintDetailsPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
-
+import FourZeroFourPage from './pages/FourZeroFourPage.jsx';
 import ReviewsPage from './pages/ReviewsPage.jsx';
 import ListReviewsPage from './pages/ListReviewsPage.jsx';
+import ContactPage from './pages/ContactPage.jsx'
+
 
 function App() {
   return (
-    <>
-      <Routes>
-        {/* Main root route */}
-        <Route path="/" element={
-          <>
-           <Navbar />
-            <MissionStatement />
-            <Appointments />
-            <Services />
-          </>
-        }/>
-        {/* Route for Leaving a review */}
-        <Route path="/leave_review" element={
-          <>
-            <Navbar />
-            <ReviewsPage />
-          </>
-        }/>
-        {/* Route for viewing list of reviews */}
-        <Route path="/list_reviews" element={
-          <>
-            <Navbar />
-            <ListReviewsPage />
-          </>
-        }/>
-
-        <Route path="/contact" element={
-          <>
-            <Navbar />
-            <ContactPage />
-          </>
-        }/>
-
-        <Route path="/CustPaintPage" element={
-          <>
-            <Navbar /> 
-            <CustPaintPage />
-          </>
-            
-        }/>
-
-        <Route path="/PaintDetailsPage" element={
-          <>
-            <Navbar /> 
-            <PaintDetailsPage />
-          </>
-            
-        }/>
-          
-        <Route path='/appointments' element={
-          <>
-          <Navbar />
-          <AppointmentsPage />
-          </>
-        } />
-        <Route path='/location' element={
-          <>
-          <Navbar />
-          <LocationPage />
-          </>
-        } />
-        <Route path='/AboutPage' element={
-          <>
-          <Navbar />
-          <AboutPage />
-          </>
-        } />
-       
-
-
-        <Route path="/admin" element={<AdminHomePage />} />
-        <Route path='/adminreviews' element={<AdminReviewsPage />} />
-        <Route path="/adminappointments" element={<AdminAppointmentsPage />} />
-
-      </Routes>  
-      </>
+    <>     
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<> <MissionStatement /> <Appointments /> <Services /> </>} />          
+          <Route path='/leave_review' element={<ReviewsPage />} />
+          <Route path='/list_reviews' element={<ListReviewsPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/custPaintPage' element={<CustPaintPage />} />
+          <Route path='/paintDetailsPage' element={<PaintDetailsPage />} />
+          <Route path='/appointments' element={<AppointmentsPage />} />
+          <Route path='/location' element={<LocationPage />} />
+          <Route path='/aboutPage' element={<AboutPage />} />
+          <Route path='/admin' element={<AdminHomePage />} />
+          <Route path='/adminreviews' element={<AdminReviewsPage />} />
+          <Route path='/adminappointments' element={<AdminAppointmentsPage />} />
+          <Route path='*' element={<FourZeroFourPage />} />
+        </Routes>    
+        {/* Add you Footer component here  */}      
+    </>
   )
 }
 export default App
