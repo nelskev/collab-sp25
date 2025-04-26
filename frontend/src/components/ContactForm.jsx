99e97c1708346f1b2acf1c4a67c8bdf40b28ee18
VladKolmorgan <kolmorganvlad@gmail.com>
2025-04-26 14:55:02 -0600

    Added Updated Contact Validation
    - Added Error pop ups to ContactForm.jsx
    - Added Errors pop ups to ContactPage.jsx
    - Moved contactValidation.js (frontend) to admin validation (wasn't in SRC so it wasn't accessible?)
    - Updated contactValidation.js (frontend) to be more uniform with other form error validation
    - Updated contactValidation.js (backend) to reflect frontend validation

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -6,6 +6,11 @@ function ContactForm({
     phone, setPhone,
     details, setDetails,
     handleSubmit,
+    // allErrors,
+    nameError,
+    emailError,
+    phoneError,
+    detailsError
 }) {
     return (
         <form onSubmit={handleSubmit} className="p-2 my-3 col-12 col-med-9 col-lg-12 col-xl-5 mx-auto">
@@ -16,19 +21,23 @@ function ContactForm({
 
             <div className="mb-3">
                 <label className="form-label">Name</label>
-                <input type="text" className="form-control" placeholder="John Smith" value={name} onChange={(e) => setName(e.target.value)} required />
+                <input type="text" className="form-control" placeholder="John Smith" value={name} onChange={(e) => setName(e.target.value)} />
+                {nameError && <span className="text-danger">{nameError}</span>} 
             </div>
             <div className="mb-3">
                 <label className="form-label">Email</label>
-                <input type="email" className="form-control" placeholder="johnsmith@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type="email" className="form-control" placeholder="johnsmith@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                {emailError && <span className="text-danger">{emailError}</span>}
             </div>
             <div className="mb-3">
                 <label className="form-label">Phone</label>
-                <input type="tel" className="form-control" placeholder="1234567890" pattern="\d{7,15}$" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                <input type="tel" className="form-control" placeholder="123-456-7890" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                {phoneError && <span className="text-danger">{phoneError}</span>}
             </div>
             <div className="mb-3">
                 <label className="form-label">Details</label>
-                <textarea className="form-control" placeholder="I am contacting because..." value={details} onChange={(e) => setDetails(e.target.value)} required />
+                <textarea className="form-control" placeholder="I am contacting because..." value={details} onChange={(e) => setDetails(e.target.value)} />
+                {detailsError && <span className="text-danger">{detailsError}</span>}
             </div>
             
             <button type="submit" className="btn btn-primary w-100 fs-5 mt-3 mb-2">
