eaf7dd35c18cdb7d01e0eae5fe6a297dab792ab9
Kevin <kevinnelson2@my.cwi.edu>
2025-04-30 09:36:52 -0600

    Deleted AdminCreatePage.jsx, deleted unnecessary import on AdminHomePage, and removed excess comments on ProtectedRoute.jsx.

--- frontend/src/admin/pages/AdminHomePage.jsx
+++ frontend/src/admin/pages/AdminHomePage.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react'
 import AdminNavbar from '../components/AdminNavbar';
-import { authenticatedFetch } from '../authentication/authenticatedFetch'
 
 // This page WILL NEED TO change browser tab title to 'ADMIN : Scotts Collision Repair'
 
