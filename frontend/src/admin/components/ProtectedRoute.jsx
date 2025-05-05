f5b356932794252c8d3cc351e2e7e5480fcf0978
kevin <youremail@yourdomain.com>
2025-05-05 14:18:21 -0600

    Removed forgotten localhost fetch

--- frontend/src/admin/components/ProtectedRoute.jsx
+++ frontend/src/admin/components/ProtectedRoute.jsx
@@ -24,7 +24,7 @@ const ProtectedRoute = ({ children }) => {
       }
 
       try {
-        const response = await fetch('http://localhost:8000/admin/signin/verify-token', {
+        const response = await fetch('/admin/signin/verify-token', {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
